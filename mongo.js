54b466afca2de40f051743d2dcc72947e54ca9a3
Universami <sami.toiskallio@protonmail.com>
2019-11-20 18:03:01 +0200

    add a mock file to test the connection to the database

---
+++ mongo.js
@@ -0,0 +1,29 @@
+
+/*
+  Mock file to test the database
+*/
+
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+const url = process.env.DB_URI
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const bookSchema = new mongoose.Schema({
+  name: String,
+  author: String,
+})
+
+const Book = mongoose.model('Book', bookSchema)
+
+const book = new Book({
+  name: 'Adventures of MrMerge collection',
+  author: 'MrMerge'
+})
+
+book.save().then(response => {
+  console.log('book saved!');
+  mongoose.connection.close();
+})
