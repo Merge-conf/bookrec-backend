042927a3fda2dd96df0df88e2896db30a63457c9
darake <adamson.gert@hotmail.com>
2019-11-22 10:45:52 +0200

    remove extra mongoose connection

--- models/book.js
+++ models/book.js
@@ -1,19 +1,5 @@
 
 const mongoose = require('mongoose')
-const logger = require('../utils/logger')
-
-const url = process.env.DB_URI
-
-logger.info('connecting to', url)
-
-mongoose
-  .connect(url, { useNewUrlParser: true })
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
 
 const bookSchema = new mongoose.Schema({
   name: String,
