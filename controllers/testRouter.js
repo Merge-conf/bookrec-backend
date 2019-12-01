9774864ab6064558f424738b8ad638ceb013f838
Gert Adamson <adamson.gert@hotmail.com>
2019-12-01 13:28:51 +0200

    add audio reset to test router

--- controllers/testRouter.js
+++ controllers/testRouter.js
@@ -1,8 +1,10 @@
 const testRouter = require('express').Router()
 const Book = require('../models/book')
+const Audio = require('../models/audio')
 
 testRouter.post('/reset', async(req, res) =>{
   await Book.deleteMany({})
+  await Audio.deleteMany({})
 
   res.status(204).end()
 })
